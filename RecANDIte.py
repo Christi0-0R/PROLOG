2b9fc6f612f63323c364b17a0e594eefa6dcdc46
Christi0-0 <l23120492@morelia.tecnm.mx>
2026-08-25 22:58:34 -0600

    Opciones

--- RecANDIte.py
+++ RecANDIte.py
@@ -1,8 +1,13 @@
 # Recurividad e Iteraciones en Python (ง🔥ﾛ🔥)ง
 
+# Idea de como se puede implementar diferentes maneras de realizar el cambio:
+# Siguiendo la idea de utilizar el menor numero de monedas a mayor
+# se quitara la modena de mayor valor
+
 arr = [10, 5, 2, 1], [0, 0, 0, 0]
 
 def Recursivo(n):
+
     if n >= 10:
         n = n - 10
         arr[1][0] += 1
@@ -20,6 +25,23 @@ def Recursivo(n):
         return arr
     elif n == 0:
         return arr
+
+def Op2(r):
+
+    if r[1][0] > 0:
+        r[1][1] = r[1][1] + r[1][0]*2
+        r[1][0] = 0
+        
+    return r
+
+def Op3(r):
+
+    if r[1][2] > 0:
+        r[1][3] = r[1][3] + r[1][2]*2
+        r[1][2] = 0
+    
+    return r
+
 
 def Iterativo(n):
 
@@ -39,26 +61,40 @@ def main():
     dinero_txt = input("Dinero que se recibe: ")
     dinero = int(dinero_txt)
 
-    cambio = Recursivo(dinero) 
+    cambio1 = Recursivo(dinero)
+    cambio2 = [cambio1[0].copy(), cambio1[1].copy()]
+    Op2(cambio2)
+    cambio3 = [cambio2[0].copy(), cambio2[1].copy()]
+    Op3(cambio3)
 
     print("\nSu cambio es:")
 
-    for i in range(len(cambio[0])):
-        if cambio[1][i] > 0:
-            print(f"  ${cambio[0][i]} x {cambio[1][i]}")
+    for opcion, cambio in enumerate([cambio1, cambio2, cambio3], 1):
+
+        print(f"\nOpción {opcion}:")
+
+        for i in range(len(cambio[0])):
+            if cambio[1][i] > 0:
+                print(f"  ${cambio[0][i]} x {cambio[1][i]}")
 
     # Modo iterativo o.o
 
     dinero_txt = input("Dinero que se recibe: ")
     dinero = int(dinero_txt)
 
-    cambio = Iterativo(dinero) 
+    cambio1 = Iterativo(dinero)
+    cambio2 = [cambio1[0].copy(), cambio1[1].copy()]
+    Op2(cambio2)
+    cambio3 = [cambio2[0].copy(), cambio2[1].copy()]
+    Op3(cambio3)
 
-    print("\nSu cambio es:")
+    for opcion, cambio in enumerate([cambio1, cambio2, cambio3], 1):
 
-    for i in range(len(cambio[0])):
-        if cambio[1][i] > 0:
-            print(f"  ${cambio[0][i]} x {cambio[1][i]}")
+        print(f"\nOpción {opcion}:")
+
+        for i in range(len(cambio[0])):
+            if cambio[1][i] > 0:
+                print(f"  ${cambio[0][i]} x {cambio[1][i]}")
 
 if __name__ == '__main__':
     main()
