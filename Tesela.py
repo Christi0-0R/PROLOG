3b800b1b94267a74e4d5319ac51c7237bd197ace
Christi0-0 <l23120492@morelia.tecnm.mx>
2026-09-07 17:48:24 -0600

    avance

--- Tesela.py
+++ Tesela.py
@@ -1,3 +1,18 @@
+# Debe ser recursivo u-u
+
+# Cada tesela sera una lista de desplazamiento dx y dy respecto a x y
+
+#  Tesela 1                 Tesela 2                Tesela 3               Tesela 4
+#   (0,0)                          (1,0)              (0,0) (1,0)           (0,0) (1,0)
+#   (0,1) (1,1)              (0,1) (1,1)              (0,1)                       (1,1)
+
+# Ya no se usaran como funciones sino como un arreglo
+teselas = {
+    1: [(0,0), (0,1), (1,1)],
+    2: [(1,0), (0,1), (1,1)],
+    3: [(0,0), (1,0), (0,1)],
+    4: [(0,0), (1,0), (1,1)],
+}
 
 matriz = [[0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0],
@@ -8,87 +23,68 @@ matriz = [[0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0]]
 
-# matis 8x8
-
-# una Tesela debera tener una estructura similar a:
-#
-#  Tesela 1
-#              posicion[0][0]
-#             posicion[0][1] posicion[1][1]
-#
-#  Tesela 2
-#                             posicion[1][0]
-#             posicion[0][1] posicion[1][1]
-#
-#  Tesela 3
-#             posicion[0][0] posicion[1][0]
-#              posicion[0][1] 
-#
-#
-#  Tesela 4
-#             posicion[0][0] posicion[1][0]
-#                             posicion[1][1]
-#
-#######################################################
-#######################################################
+# matris 8x8 uwu)r
 
 
-def buscar_tesela_1(x, y):
 
-    if ((matriz[x][y] == 0)
-        and (matriz[x][y+1] == 0)
-        and (matriz[x+1][y+1] == 0)):
+def cabe(x, y, tesela): # funcion para saber si la tesela cabe dentro de la matriz
 
-        matriz[x][y] = 1
-        matriz[x][y+1] = 1
-        matriz[x+1][y+1] = 1
+    for dx, dy in tesela: #porque es una matriz
+        nx = x + dx
+        ny = y + dy
 
-        return True
-
-    return False
-
-def buscar_tesela_4(x, y):
-
-    if ((matriz[x][y] == 0)
-        and (matriz[x+1][y] == 0)
-        and (matriz[x+1][y+1] == 0)):
-
-        matriz[x][y] = 1
-        matriz[x+1][y] = 1
-        matriz[x+1][y+1] =1
-
-        return True
-
-    return False
-
-# =========================
-
-def despintar_tesela_1(x, y):
-
-    matriz[x][y] = 0
-    matriz[x][y + 1] = 0
-    matriz[x + 1][y + 1] = 0
+        if nx >= 8 or ny >= 8 or matriz[nx][ny] != 0:
+            return False
 
 
-def despintar_tesela_4(x, y):
-
-    matriz[x][y] = 0
-    matriz[x+1][y] = 0
-    matriz[x+1][y+1] = 0
-
-
-# ==========================
-
-def pintar(x, y): # pintar toda la matriz con teselas menos un cuadrito
+def pintar_tesela(x, y, tesela, valor): # pintar toda la matriz con teselas menos un cuadrito
 
     # u-u
 
+    for dx, dy in tesela:
+        matriz[x + dx][y + dy] = valor
+
+
+def siguiente(x, y):
+    # devuelve la siguiente posicion a revisar recorriendo cada fila
+
+    if y == 7:
+        return x + 1, 0
+
+    return x, y + 1
+
+
+def pintar(x, y):
+    # debe ser recursivo ¯\_(ツ)_/¯
+    
     if x == 8: # Matriz recorrida 100%
 
         # comprobacion sjd
-        cantidad_ceros = 0
+        cantidad_ceros = sum(fila.count(0) for fila in matriz)
+        return cantidad_ceros == 1
 
-        for i in range(8):
+    # si esta la casilla ya pintada salta a la siguiente
+    if matriz[x][y] != 0:
+        nx, ny = siguiente(x, y)
+        return pintar(nx, ny)
+
+    # Probar las 4 teselas en esta casilla
+    for numero, tesela in teselas.items():
+        if cabe(x, y, tesela):
+            pintar_tesela(x, y, tesela, numero)
+
+            nx, ny = siguiente(x, y)
+            if pintar(nx, ny):
+                return True
+
+            # si no funciona lo quita y prueba la siguiente tesela Bv
+            pintar_tesela(x, y, tesela, 0)
+
+    return False
+
+
+
+'''        for i in range(8):
             for j in range(8):
                 
                 if matriz[i][j] == 0:
@@ -136,14 +132,19 @@ def pintar(x, y): # pintar toda la matriz con teselas menos un cuadrito
             despintar_tesela_4(x, y)
 
     return False
+'''
+
+
+
 
 
 def main():
 
-    print (matriz)
-
-    print (pintar(0,0))
-
+    if pintar(0, 0):
+        for fila in matriz:
+            print(fila)
+    else:
+        print("No se encontro solucion :c")
 
 
 if __name__=="__main__":
